5ffe706ef9af66cdab41ff65aedc6177988d34bd
nhattien7701vn <nhattien7701vn@gmail.com>
2023-06-05 11:09:10 +0700

    update text

--- src/assets/components/Main/Home/index.jsx
+++ src/assets/components/Main/Home/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='home-leftContent'>
         <h1 className='home-name'>Hi, I'm Tien Hoang</h1>
         <h2 className='home-position'>Frontend Developer</h2>
-        <span className='home-introduce'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere inventore doloremque neque sed? Ab neque quod nisi, natus, officia dolore adipisci nulla dolor, illum nostrum exercitationem.</span>
+        <span className='home-introduce'>Hey there! I'm a Front-End Developer who's still navigating through the vast world of coding. Think of me as a friendly and approachable tech enthusiast. I may not have a ton of experience yet, but I make up for it with my knack for making people laugh. I'm always up for a good chat and making new friends, so don't hesitate to say hello. When I'm not busy coding, you can probably find me trying to score goals on a virtual soccer field or working up a sweat in a real game. Life's all about balance, right? So let's connect, have some fun, and create awesome things together !</span>
         <div className="home-button">
           <button className='btn-hireMe'>Hire me</button>
           <button className='btn-letsTalk'>Let's Talk</button>
